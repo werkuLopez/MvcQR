92f0fcfb70f428b52ff2f3913e2fd76399114741
werkuLopez <werku.lopez@outlook.es>
2024-04-02 20:11:47 +0200

    Add project files.

---
+++ MvcQR/Controllers/QRController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcQR.Helpers;
+
+namespace MvcQR.Controllers
+{
+    public class QRController : Controller
+    {
+        public HelperQR helper;
+
+        public QRController(HelperQR helper)
+        {
+            this.helper = helper;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(string texto)
+        {
+            if (texto == null)
+            {
+                ViewData["mensaje"] = "Introduzca un texto";
+                return View();
+            }
+            string qr = await this.helper.GenerarQR(texto);
+            ViewData["qr"] = qr;
+
+            return View();
+        }
+    }
+}
---
+++ MvcQR/Helpers/HelperQR.cs
@@ -0,0 +1,28 @@
+﻿using QRCoder;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcQR.Helpers
+{
+    public class HelperQR
+    {
+
+        public async Task<string> GenerarQR(string texto)
+        {
+            var qrGenerator = new QRCodeGenerator();
+            var qrData = qrGenerator.CreateQrCode(texto, QRCodeGenerator.ECCLevel.Q);
+            BitmapByteQRCode bitmapByteQRCode = new BitmapByteQRCode(qrData);
+            var bitMap = bitmapByteQRCode.GetGraphic(20);
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                ms.Write(bitMap);
+
+                byte[] byteImg = ms.ToArray();
+                string base64String = Convert.ToBase64String(byteImg);
+
+                return base64String;
+            }
+        }
+    }
+}
